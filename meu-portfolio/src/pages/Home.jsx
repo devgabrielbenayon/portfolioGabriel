1e792a19a9bccc5ca7671d92c8b1d6ba547acd0a
Gabriel Benayon <gabrielbenayon16@gmail.com>
2025-07-14 15:13:11 -0400

    First steps

---
+++ meu-portfolio/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Olá! Eu sou o Gabriel</h1>
+      <p>Bem vindo ao meu portfólio!</p>
+    </div>
+  );
+}
+
+export default Home;
